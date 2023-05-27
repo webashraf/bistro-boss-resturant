362220975a3abdb9f468af396abf1690eed47cad
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-27 15:40:32 +0600

    add some code

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -4,11 +4,12 @@ const useMenu = () => {
     const [menus, setMenus] = useState([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-      fetch("./menu.json")
+      fetch("http://localhost:5000/menu")
         .then((res) => res.json())
         .then((data) => {
           setMenus(data);
           setLoading(false);
+          console.log(data);
         });
     }, []);
     return [menus, loading];
