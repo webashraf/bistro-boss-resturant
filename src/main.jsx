a90421e034006a022c4914452d1a0d1731b7aabd
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-29 21:18:58 +0600

    add useCart hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import router from './routes/router.jsx'
-import AuthProvider from './authProvider/AuthProvider'
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./authProvider/AuthProvider";
+import "./index.css";
+import router from "./routes/router.jsx";
+const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<AuthProvider>
-<div className='max-w-screen-xl mx-auto w-full'>
-    <RouterProvider router={router} />
-</div>
-</AuthProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <div className="max-w-screen-xl mx-auto w-full">
+          <RouterProvider router={router} />
+        </div>{" "}
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
