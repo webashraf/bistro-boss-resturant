6f2edb9451621b7749adf091db6a7cff5e32f3ea
webashraf <aliashraftamim@gmail.com>
2023-06-01 18:34:30 +0600

    add some code

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -8,8 +8,6 @@ const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider;
 
 
-
-
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
