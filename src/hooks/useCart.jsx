0fd633c182ceba421584e83daeb2e49eeeb3f1f0
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-06-01 18:09:18 +0600

    add some code

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -11,8 +11,10 @@ const useCart = () => {
     const {refetch, data : cart = [] } = useQuery({
         queryKey: ['cart', user?.email],
         queryFn: async ()=>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
-            return res.json();
+
+                const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
+                return res.json();
+            
         }
       })
 
